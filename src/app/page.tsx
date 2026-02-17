7176a59b500a6e03bcd2a1e4ae69fad1abe508a6
Praveen Kumar <praveen.kumar.tek@gmail.com>
2026-02-17 10:31:24 +0530

    auth fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,15 @@
-import { redirect } from 'next/navigation'
+import { auth } from "@/lib/auth"
+import { headers } from "next/headers"
+import { redirect } from "next/navigation"
 
-export default function Page() {
-  redirect('/home')
+export default async function ServerComponent() {
+  const session = await auth.api.getSession({
+    headers: await headers()
+  })
+  if (!session) {
+    redirect("/home")
+  }
+  return (
+    redirect("/dashboard")
+  )
 }
